7b3cde7d9a7f2590aa9695490a1af3d8302f2dfb
subhampaul2002 <102228424+subhampaul2002@users.noreply.github.com>
2022-04-18 22:25:08 +0530

    Add files via upload

---
+++ mainscript.js
@@ -0,0 +1,24 @@
+var key = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+	var pianoKey=['','a', 'w', 's', 'e', 'd', 'f', 't', 'g', 'y', 'h', 'u','j', 'k', 'o','l', 'p', 'z', 'x', 'c'];
+	var audioPlay = ['c1', 'c1_sharp', 'd1', 'd1_sharp', 'e1', 'f1', 'f1_sharp', 'g1', 'g1_sharp', 'a1', 'a1_sharp', 'b1', 'c2', 'c2_sharp', 'd2', 'd2_sharp', 'e2'];
+	$(document).ready(function () {
+		$("div").click(function () {
+			var id = $(this).attr("id");
+			//console.log(id);
+			document.getElementById('a_' + id.replace("id_", "")).play();
+			//console.log(i);
+		});
+	});
+	
+	$(document).ready(function () {
+	$(document).bind('keypress', function (e) {
+		var character = String.fromCharCode(e.keyCode || e.which);
+		//console.log(character);
+		for (var i = 0; i < pianoKey.length; i++) {
+			if (pianoKey[i] == character) {
+				console.log(i);
+				document.getElementById('a_' + i).play();
+			}
+		}
+	});
+});
